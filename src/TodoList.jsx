002b0ad7886e6ba296663d5339808fe4c6de188b
Shuna Li <shuna.li@hotmail.com>
2026-07-13 18:11:44 -0700

    adding assigment 2

---
+++ src/TodoList.jsx
@@ -0,0 +1,14 @@
+const TodoList = () => {
+  const todoList = [
+    { id: 1, title: "Review resources" },
+    { id: 2, title: "Take notes" },
+    { id: 3, title: "Code out app" },
+  ];
+  return (
+    <ul>
+      {todoList && todoList.map((todo) => <li key={todo.id}>{todo.title}</li>)}
+    </ul>
+  );
+};
+
+export default TodoList;
